e3fbd6bb6d3e9a281a9575b19a58095623e311f9
matt6341 <matt6341@ISS147D8-09.scdsb.pri>
2014-10-02 08:42:23 -0400

    Message

---
+++ src/JARFILES/MakeEmployees.java
@@ -0,0 +1,11 @@
+package JARFILES;
+import TerminalIO.*;
+
+public class MakeEmployees {
+    public static void main(String[] args){
+        KeyboardReader kr = new KeyboardReader();
+        String name = kr.readLine("Enter Name: ");
+        System.out.println("Your name is: " + name);
+    }
+    
+}
